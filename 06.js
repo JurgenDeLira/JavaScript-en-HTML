edb49ea665e32a66462f02ca118576211e541853
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-03-01 11:23:19 -0700

    Add files via upload

---
+++ 06.js
@@ -0,0 +1,15 @@
+// Objeto Math 
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.7);
+resultado = Math.ceil(2.2); // Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.8); // floor hacia abajo
+resultado = Math.sqrt(144); // Raíz cuadrada
+resultado = Math.abs(-200); // convierte números negativos a positivos
+resultado = Math.min( 3, 5,  1, 8, 2, 10); // el menor de una lista
+resultado = Math.random();
+resultado = Math.floor( Math.random() * 30);
+
+
+console.log(resultado);
